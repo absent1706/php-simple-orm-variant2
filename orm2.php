a4a28f0d88a4e59fd75b5b7ef06af2a7eff9a7b6
Alexander Litvinenko <litvinenko1706@gmail.com>
2016-05-08 15:20:07 +0300

    + Query Builder

--- orm2.php
+++ orm2.php
@@ -3,21 +3,25 @@
 abstract class DbModel
 {
     protected static $table;
+    protected static $conn;
 
-    public static function all()
+    public static function setConnection($conn)
+    {
+        self::$conn = $conn;
+    }
+
+    public static function table()
+    {
+        // TODO: auto guess table name based on class name
+        $class = get_called_class();
+        return $class::$table;
+    }
+
+    public static function all($conn)
     {
         /* query  begin */
-            $connection_string = "mysql:host=localhost;dbname=test_db";
-            $user = 'root';
-            $password = '';
-            $conn = new PDO($connection_string, $user, $password);
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-
-            $currentClass = get_called_class();
-            $stmt = $conn->prepare("select * from ".$currentClass::$table);
-            $stmt->execute();
-            return $stmt->fetchAll(PDO::FETCH_CLASS, 'Post');
+        $results = (new QueryBuilder(self::$conn, self::table()))->get();
+        return $results;
         /* query  end */
 
 
@@ -27,15 +31,40 @@ abstract class DbModel
 class Post extends DbModel
 {
     protected static $table = 'posts';
+}
 
-
+class User extends DbModel
+{
+    protected static $table = 'users';
 }
 
 class QueryBuilder
 {
+    public function get()
+    {
+        $stmt = $this->conn->prepare($this->query);
 
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
+
+    public function __construct($conn, $table)
+    {
+        $this->conn = $conn;
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $this->query = "SELECT * FROM $table";
+    }
 }
 
 
-// var_dump(Post::all());
-var_dump(Post::all()[0]->title);
+// var_dump(Post::all()[0]->title);
+
+$connection_string = "mysql:host=localhost;dbname=test_db";
+$user = 'root';
+$password = '';
+$conn = new PDO($connection_string, $user, $password);
+
+// var_dump((new QueryBuilder($conn, 'posts'))->get());
+DbModel::setConnection($conn);
+var_dump(Post::all());
